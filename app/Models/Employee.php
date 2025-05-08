0e356c372d4a4f283cb07c45517e126607149f29
RobertoRuben <robertoch263@gmail.com>
2025-05-08 10:35:06 -0500

    ✨ feat(model): add Employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'dni',
+        'last_name',
+        'middle_name',
+        'first_names',
+        'department_id',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->last_name} {$this->middle_name}, {$this->first_names}";
+    }
+}
