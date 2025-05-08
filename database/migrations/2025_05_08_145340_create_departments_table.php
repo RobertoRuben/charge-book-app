a3b4e4cabe5b4081a6766ee7628dcce06ad7b726
RobertoRuben <robertoch263@gmail.com>
2025-05-08 10:05:48 -0500

    ♻️ refactor(migrations): change department name column type to string

--- database/migrations/2025_05_08_145340_create_departments_table.php
+++ database/migrations/2025_05_08_145340_create_departments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->text('name')->unique();
+            $table->string('name', 255)->unique();
             $table->timestamps();
         });
     }
